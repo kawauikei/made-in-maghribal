58a28ab568c6cc46bd9f3b4a383b6a07f10fefde
kawazu <neko.qv@gmail.com>
2026-05-01 13:57:55 +0900

    M-QUIZ-PROMPT-TUNING-CONT: Remove redundant accessory prefix in choice labels

--- main.js
+++ main.js
@@ -9649,7 +9649,6 @@ function App() {
         if (category === "DAY") displayChoiceName = `一般雑貨の${displayChoiceName}`;
         if (category === "TRD") displayChoiceName = `貿易品の${displayChoiceName}`;
         if (category === "RIT") displayChoiceName = `厳かな${displayChoiceName}`;
-        if (category === "ADN") displayChoiceName = `アクセサリーの${displayChoiceName}`;
       }
       return /* @__PURE__ */ React.createElement(
         "div",
